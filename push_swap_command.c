4bfa9c46b4383539f3e3f71eda1065e43eacf5c9
Safwaan Noor <sanoor@c3r1s8.42london.com>
2024-03-16 22:34:26 +0000

    added

--- push_swap_command.c
+++ push_swap_command.c
@@ -1,33 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   push_swap_command.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sanoor <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/13 20:13:53 by sanoor            #+#    #+#             */
-/*   Updated: 2024/03/16 22:34:08 by sanoor           ###   ########.fr       */
+/*   Created: 2024/03/16 22:33:49 by sanoor            #+#    #+#             */
+/*   Updated: 2024/03/16 22:34:04 by sanoor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP
-# define PUSH_SWAP
+#include "push_swap.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
 
-typedef struct s_stack_node
-{
-	int					value;
-	int					current_position;
-	int					final_index;
-	int					push_price;
-	bool				above_median;
-	bool				cheapest;
-	struct s_stack_node	*target_node;
-	struct s_stack_node	*next;
-	struct s_stack_node	*prev;
-}				t_stack_node;
-
-endif
---
+++ tiny_sort.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tiny_sort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanoor <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 17:55:07 by sanoor            #+#    #+#             */
+/*   Updated: 2024/03/15 18:48:37 by sanoor           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.c"
+
+bool	stack_sorted(t_stack_node *stack)
+{
+	if (NULL == stack)
+		return (1);
+	while (stack->next)
+	{
+		if (stack->value > stack->next->value)
+			return (false);
+		stack = stack->next;
+	}
+	return (true);
+}
+
+static t_stack_node	*find_highest(t_stack_node *stack)
+{
+	int				highest;
+	t_stack_node	*highest_node;
+
+	if (stack == NULL)
+		return (NULL);
+	highest = INT_MIN;
+	while (stack)
+	{
+		if (stack->value > highest)
+		{
+			highest = stack->value;
+			highest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (highest_node);
+}
+
+void	tiny_sort(t_stack_node **a)
+{
+	t_stack_node	*highest_node;
+
+	highest_node = find_highest(*a);
+	if (*a == highest_node)
+		ra(a);
+	else if ((*a)->next == highest_node)
+		rra(a);
+	if ((*a)->value > (*a)->next->value)
+		sa(a);
+}
+
+void	handle_five(t_stack_node **a, t_stack_node **b)
+{
+	while (stack_len(*a) > 3)
+	{
+		init_nodes(*a, *b);
+		finish_rotation(a, find_smallest(*a), 'a');
+		pb(b, a, false);
+	}
+}
